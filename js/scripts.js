5b9778e76f94ed294d91539500fb96d3a97c6e54
esc002 <esc@ad.unc.edu>
2024-09-16 10:36:54 -0400

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// For the checkboxes since using CSS on forms wasn't working for me.
+function changeImage() {
+    var img = document.getElementById('image');
+    img.src = '../images/custom-checkbox.png';
+}
